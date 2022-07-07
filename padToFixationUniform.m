eb190076382cb80b5df3911f927890fa966637d8
Vasha DuTell <vashadutell@gmail.com>
2022-07-07 15:18:20 -0400

    fixed uniform pooling region, centering and saving padded image with pool region visualization

--- padToFixationUniform.m
+++ padToFixationUniform.m
@@ -1,5 +1,5 @@
 function [paddedimg, fx, fy, poolingRegions, padding] = ...
-    padToFixation(img, fixPt, foveaSize, poolingRate, radialOverlap, numAngular, padval, out_dir)
+    padToFixationUniform(img, fixPt, foveaSize, poolingRate, radialOverlap, numAngular, padval, out_dir)
 % [paddedimg, fx, fy, poolingRegions] = padToFixation(img, fixPt, foveaSize, 
 %       poolingRate, radialOverlap, numAngular, padval, out_dir)
 %
@@ -35,9 +35,8 @@ if nargin == 7
     out_dir = [];
 end
 
-sprintf('test')
-r = fixPt
-
+fx = fixPt(1); fy = fixPt(2);
+r = fixPt(1);
 [h,w,d] = size(img);
 
 %if not(isempty(fx) | isempty(fx))
@@ -65,9 +64,18 @@ s = round(r*poolingRate); % diameter
 a = round(s/2); b = s-a; % a is radius of pooling region. b is also radius plus spill-over
 buff = max(3,ceil(a/4)); % buffer for edge of pooling region
 
-% get centers of each pooling region
-px = round(1:0.75*s:w);
-py = round(1:0.75*s:h);
+% get centers of each pooling region: center regions at img center
+img_center = [round(w/2),round(h/2)];
+px_left = round(img_center(1):-(1-radialOverlap)*s:1-a);
+px_right = round(img_center(1):(1-radialOverlap)*s:w+b);
+px = cat(2,flip(px_left),px_right(2:end));
+py_left = round(img_center(2):-(1-radialOverlap)*s:1-a);
+py_right = round(img_center(2):(1-radialOverlap)*s:h+b);
+py = cat(2,flip(py_left),py_right(2:end));
+
+%old code to center on upper left edge
+%px = round(1:(1-radialOverlap)*s:w+b);
+%py = round(1:(1-radialOverlap)*s:h+b);
 [p_x,p_y] = meshgrid(px,py);
 num_pools_x = length(px);
 num_pools_y = length(py);
@@ -87,22 +95,21 @@ end_x = p_x + b + buff;
 start_y = p_y - a - buff + 1;
 end_y = p_y + b + buff;
 
-min_x = min(start_x)
-max_x = max(end_x)
-min_y = min(start_y)
-max_y = max(end_y)
+min_x = min(start_x);
+max_x = max(end_x);
+min_y = min(start_y);
+max_y = max(end_y);
 
 % pad[Left/Right/Bot/Top] is the amount to add in a particular direction
 % for min_x and min_y, add a pixel; the minimum will be 
 % something like -2, which means we need to account for -2, -1, and 0 
 % before we get to the image at 1, ...
-padLeft = abs(min_x)+1
-padTop = abs(min_y)+1
+padLeft = abs(min_x)+1;
+padTop = abs(min_y)+1;
 % for max_x and max_y, just pad by the amount that the max is greater than
 % the current dimension. E.G., if max is 5 and w is 3, then pad by 2
-padRight = max_x-w
-padBot = max_y-h
-
+padRight = max_x-w;
+padBot = max_y-h;
 
 % actually pad here:
 for i=1:d,
@@ -117,16 +124,18 @@ padding = [padLeft padRight padTop padBot];
 % now that the image is padded, need to add the padding to fx, fy, and 
 % the pooling regions, to convert them to their new pixel values
 
-poolingRegions = [p_x,p_y,repmat(a,length(p_x)),repmat(b,length(p_x)),repmat(buff,length(p_x))];
+numPoolingRegions = length(p_x);
+
+poolingRegions = [p_x,p_y,repmat(a,numPoolingRegions),repmat(b,numPoolingRegions),repmat(buff,numPoolingRegions)];
 
 poolingRegions(:,1) = poolingRegions(:,1) + padLeft;
-poolingRegions(:,2) = poolingRegions(:,2) + padRight;
+poolingRegions(:,2) = poolingRegions(:,2) + padTop;
 
 % mark pooling region boundaries with buffer zone added and plot on screen
 [H, W, ~] = size(paddedimg);
-figure; image(paddedimg); axis('image');
-% length(poolingRegions);
-for itp1=1:length(p_x)
+figure; image(paddedimg); 
+hold on;
+for itp1=1:numPoolingRegions
     buffPixels = poolingRegions(itp1,5);            
         p_x = poolingRegions(itp1, 1);
         p_y = poolingRegions(itp1, 2);
@@ -143,8 +152,12 @@ for itp1=1:length(p_x)
             p_y+b+buffPixels p_y+b+buffPixels]';
         line(X, Y,'LineWidth',1)  
 end
+axis('image');
+hold off;
 % save a picture of the pooling regions 
 save_name = strcat(out_dir,'/poolingRegion.png');
-saveas(gcf,save_name);
 
+[X, map] = frame2im(getframe(gcf));                                                  
+imwrite(X,save_name); 
+close all;
 return;
